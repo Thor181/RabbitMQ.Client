1e82604db462f35a9dadfbc0d378c7928ebbe51c
Артём Торгашин <art.torgaschin@yandex.ru>
2024-07-04 20:12:58 +0700

    Add project files.

---
+++ RabbitMQ.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "myQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            
+            consumer.Received += Consumer_Received;
+            channel.BasicConsume(queue: "myQueue", autoAck: true, consumerTag: "Consumer_" + Path.GetRandomFileName(), consumer: consumer);
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var messageRaw = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(messageRaw);
+
+            Console.WriteLine($" [x] Received: {message} ");
+        }
+    }
+}
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+using RabbitMQ.Client;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "myQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            const string messageLeft = "Hello_";
+
+            while (true)
+            {
+                await Task.Delay(2000);
+                var messageRight = messageLeft + Random.Shared.Next().ToString();
+                var body = Encoding.UTF8.GetBytes(messageRight);
+
+                channel.BasicPublish(exchange: string.Empty, routingKey: "myQueue", basicProperties: null, body: body);
+                Console.WriteLine($" [x] Sent {messageRight}");
+            }
+
+
+        }
+    }
+}
